89fd6da2d983c76965ea5a7ed200027bfc26d7a8
umair379 <umairkhann520@gmail.com>
2024-12-09 01:22:50 +0500

    ecommerce website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +19,31 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.className} bg-whiten`}>
+        {/* Include Header and Navbar here */}
+        <Header />
+        <Navbar />
+
+
+
+
+
+      
+        {/* Render children passed to the layout */}
         {children}
+        <Footer/>
       </body>
     </html>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
