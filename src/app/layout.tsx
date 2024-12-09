150fa17a62b3d0cdcef22bd6e5525d3b7b6dbb33
ahmerkhan99 <ahmer2942@gmail.com>
2024-12-09 22:37:48 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
-import Footer from "./components/Footer/Footer";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,19 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-whiten`}>
-        {/* Include Header and Navbar here */}
-        <Header />
-        <Navbar />
-
-
-
-
-
+      <body>
       
-        {/* Render children passed to the layout */}
         {children}
-        <Footer/>
       </body>
     </html>
   );
